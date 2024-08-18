b386f360ae75f284be1525d67105b17fdadb0a64
KimHeeSoo <khstoday@naver.com>
2024-08-18 11:43:01 +0900

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
 import ClipLoader from "react-spinners/ClipLoader";
-//1.앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
-//2.날씨정보에는 도시,섭씨,화씨의 날씨상태
-//3.5개의 버튼이 있다.(1개는 현재위치 4개는 다른 도시)
-//4.도시버튼을 클릭할때 마다 도시별 날씨가 나온다/
-//5.현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
-//6.데이터를 들고오는 동안 로딩 스피너가 돈다.
+
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
-  const [loading, setLoading] = useState("false");
+  const [loading, setLoading] = useState(false);
   const [color, setColor] = useState("green");
   const cities = ["paris", "new york", "tokyo", "seoul"];
 
-  const getCurrentLocation = () => {
+  // 현재 위치 기반 날씨 정보 가져오는 함수
+  const getCurrentLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       getWeatherByCurrentLocation(lat, lon);
       console.log("현재위치", lat, lon);
     });
-  };
+  }, []);
 
+  // 현재 위치를 기반으로 날씨 정보를 가져오는 비동기 함수
   const getWeatherByCurrentLocation = async (lat, lon) => {
     try {
       let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=43134dbd7d6b477b80e801bca836e039&units=metric`;
@@ -43,7 +40,8 @@ function App() {
     }
   };
 
-  const getWeatherByCity = async () => {
+  // 선택된 도시의 날씨 정보를 가져오는 비동기 함수
+  const getWeatherByCity = useCallback(async () => {
     try {
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=43134dbd7d6b477b80e801bca836e039&units=metric`;
       setLoading(true);
@@ -58,15 +56,15 @@ function App() {
     } finally {
       setLoading(false);
     }
-  };
+  }, [city]);
 
   useEffect(() => {
-    if (city == "") {
+    if (city === "") {
       getCurrentLocation();
     } else {
       getWeatherByCity();
     }
-  }, [city]);
+  }, [city, getCurrentLocation, getWeatherByCity]);
 
   return (
     <div>
